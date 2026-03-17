481ac470f88bff1350de26e82e956b5ac484ff1b
ywkim212 <ywkim212@gmail.com>
2026-03-17 09:29:31 +0900

    Add files via upload

---
+++ nbcamp_ch2_codekata/matter_9/main.cpp
@@ -0,0 +1,16 @@
+﻿using namespace std;
+
+int solution(int n)
+{
+    int num = 0;
+    int answer = 0;
+    while (num <= n)
+    {
+        if (num % 2 == 0)
+        {
+            answer += num;
+        }
+        num++;
+    }
+    return answer;
+}
