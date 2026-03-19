74953184a3b2bc90348fd789c19ba116f80c77a9
ywkim212 <ywkim212@gmail.com>
2026-03-20 08:57:16 +0900

    Add files via upload

---
+++ nbcamp_ch2_codekata/matter_12/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <vector>
+
+using namespace std;
+
+double solution(vector<int> numbers)
+{
+    int size = numbers.size();
+    double answer = 0;
+    for (int i = 0; i < size; i++)
+    {
+        answer += numbers[i];
+    }
+    answer /= size;
+    return answer;
+}
